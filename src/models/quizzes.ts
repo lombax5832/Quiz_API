7496f5a74bcf95da8aa5c564647d774848dded9a
Alexei Snytkine <ipodalexei@gmail.com>
2020-07-21 04:58:23 -0400

    added passing_grade field

--- src/models/quizzes.ts
+++ src/models/quizzes.ts
@@ -1,11 +1,12 @@
 import mongoose, { Schema } from "mongoose";
 
 const QuizSchema = new Schema({
-    category_id: {type: Schema.Types.ObjectId, required: true},
+    category_id: { type: Schema.Types.ObjectId, required: true },
     title: { type: String, required: true },
     slug: { type: String, required: true },
     quiz_id: { type: String },
-    description: { type: String }
+    description: { type: String },
+    passing_grade: { type: Number }
 }, {
     versionKey: false
 });
